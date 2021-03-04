83b4027b62fc67c1aca93eb0343e42ac11c66c16
Ghaith Obeidat <ghaith.ob85@gmail.com>
2021-03-04 16:52:18 +0200

    edit

---
+++ dynamic.js
@@ -0,0 +1,8 @@
+console.log('welome to ASAC');
+document.write('My name is Ghaith');
+var name = prompt('enter your name');
+if (name == "Ghaith")
+{alert('welcome Ghaith');}
+else if ( name == "Mariam")
+{alert ('welcome Mariam');}
+else {alert ('not matched');}
